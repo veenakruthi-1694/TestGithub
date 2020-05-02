b5fd3b1d6ca62005ad7197b142ee9d0b2b187bf6
veenakruthi-1694 <62419866+veenakruthi-1694@users.noreply.github.com>
2020-05-02 18:16:33 +0530

    Removing stopwords

---
+++ stopwords .py
@@ -0,0 +1,11 @@
+def remove_stopwords(text, is_lower_case=False):
+    tokens = tokenizer.tokenize(text)
+    tokens = [token.strip() for token in tokens]
+    if is_lower_case:
+        filtered_tokens = [token for token in tokens if token not in stopword_list]
+    else:
+        filtered_tokens = [token for token in tokens if token.lower() not in stopword_list]
+    filtered_text = ' '.join(filtered_tokens)    
+    return filtered_text
+
+remove_stopwords("The, and, if are stopwords, computer is not")
