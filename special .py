717a00385aaacd8ae2eba633af9d6572e9539359
veenakruthi-1694 <62419866+veenakruthi-1694@users.noreply.github.com>
2020-05-02 12:45:30 +0530

    Removing special character

---
+++ special .py
@@ -0,0 +1,7 @@
+def remove_special_characters(text, remove_digits=False):
+    pattern = r'[^a-zA-z0-9\s]' if not remove_digits else r'[^a-zA-z\s]'
+    text = re.sub(pattern, '', text)
+    return text
+
+remove_special_characters("Well this was fun! What do you think? 123#@!", 
+                          remove_digits=True)
