969595b2ab1b7ef8fdd5cdc8f40a11e5b352d976
veenakruthi-1694 <62419866+veenakruthi-1694@users.noreply.github.com>
2020-05-02 13:11:48 +0530

    stemming

---
+++ stemm .py
@@ -0,0 +1,6 @@
+def simple_stemmer(text):
+    ps = nltk.porter.PorterStemmer()
+    text = ' '.join([ps.stem(word) for word in text.split()])
+    return text
+
+simple_stemmer("My system keeps crashing his crashed yesterday, ours crashes daily")
