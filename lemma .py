8344c934039c350ad841d30f435ed2abce02182d
veenakruthi-1694 <62419866+veenakruthi-1694@users.noreply.github.com>
2020-05-02 13:19:06 +0530

    lemmatization

---
+++ lemma .py
@@ -0,0 +1,6 @@
+def lemmatize_text(text):
+    text = nlp(text)
+    text = ' '.join([word.lemma_ if word.lemma_ != '-PRON-' else word.text for word in text])
+    return text
+
+lemmatize_text("My system keeps crashing! his crashed yesterday, ours crashes daily")
